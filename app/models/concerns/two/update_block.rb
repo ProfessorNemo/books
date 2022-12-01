411f3cd6b33c72326dbc410a77b996bc138c0a61
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-12-01 17:16:02 +0300

    add request17 for update table book_editions

--- app/models/concerns/two/update_block.rb
+++ app/models/concerns/two/update_block.rb
@@ -86,6 +86,50 @@ module Two
                                                      FROM genres
                                                      WHERE name_genre = 'Поэзия')")
      }
+
+      # Для книг, которые уже есть на складе (в таблице book_editions), но по другой цене, чем в поставке (supplies),
+      # необходимо в таблице book_editions увеличить количество на значение, указанное в поставке, и пересчитать цену.
+      # А в таблице supply обнулить количество этих книг
+      scope :request17, lambda {
+        connection.select_all("WITH supply_values AS (
+                                      /*Получаем количество и цену книг из supplies до обнуления*/
+                                      SELECT
+                                        authors.author_id,
+                                        supplies.author,
+                                        supplies.title,
+                                        supplies.amount,
+                                        supplies.price
+                                      FROM
+                                        book_editions
+                                        JOIN authors USING (author_id)
+                                        JOIN supplies ON book_editions.title = supplies.title
+                                          AND name_author = supplies.author
+                                          AND book_editions.price <> supplies.price
+                                    ),
+                                    cte AS (
+                                      /*Обнуляем значение amount в supplies из рассчитанной таблицы supply_values*/
+                                      UPDATE
+                                        supplies
+                                      SET
+                                        amount = 0
+                                      FROM
+                                        supply_values sv
+                                      WHERE
+                                        supplies.author = sv.author
+                                        AND supplies.title = sv.title)
+                                    UPDATE
+                                      book_editions
+                                    SET
+                                      price = (book_editions.price * book_editions.amount + sv.price * sv.amount) / (book_editions.amount + sv.amount),
+                                      amount = book_editions.amount + sv.amount
+                                    FROM
+                                      supply_values sv
+                                    WHERE
+                                      book_editions.title = sv.title AND
+                                      book_editions.author_id = sv.author_id
+                                    RETURNING
+                                      *")
+      }
     end
   end
 end
